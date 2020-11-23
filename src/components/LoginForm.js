cf26801ccd1b1c2ad38feaf41d1c2691d73edf83
pierre-phil <pierre-philippon@live.fr>
2020-11-23 12:39:11 +0100

    add bootstrap

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,28 +3,32 @@ import React from "react";
 const LoginForm = () => {
   return (
     <section className="loginForm">
-      <div className="header">
-        <h2>Login/Register</h2>
+      <div className=" container row">
+        <div className="col-sm-12 col-md-8 col-lg-4">
+          <div className="header">
+            <h2>Login/Register</h2>
+          </div>
+          <form className="form">
+            <label htmlFor="email"></label>
+            <input
+              type="text"
+              id="email"
+              email="email"
+              required
+              placeholder="Email"
+            />
+            <label htmlFor="email"></label>
+            <input
+              type="text"
+              id="password"
+              email="password"
+              required
+              placeholder="Password"
+            />
+            <button type="submit">Connect</button>
+          </form>
+        </div>
       </div>
-      <form className="form">
-        <label htmlFor="email"></label>
-        <input
-          type="text"
-          id="email"
-          email="email"
-          required
-          placeholder="Email"
-        />
-        <label htmlFor="email"></label>
-        <input
-          type="text"
-          id="password"
-          email="password"
-          required
-          placeholder="Password"
-        />
-        <button type="submit">Connect</button>
-      </form>
     </section>
   );
 };
