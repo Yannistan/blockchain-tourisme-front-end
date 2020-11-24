b70ba72afc95e46f9de3877d9f29af46c82b3bf8
pierre-phil <pierre-philippon@live.fr>
2020-11-24 14:59:38 +0100

    create nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,10 @@ const Header = () => {
     <>
       <header>
         <h1 className="h2">Name of App</h1>
+        <nav>
+          <a href="/">Travels</a>
+          <a href="/">Account</a>
+        </nav>
       </header>
     </>
   );
