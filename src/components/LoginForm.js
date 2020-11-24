452d940aa370a7e00334eb5f3c3cf8ed912254cd
pierre-phil <pierre-philippon@live.fr>
2020-11-24 14:31:27 +0100

    start login/register box

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+  const [register, setRegister] = useState(false);
+  const [login, setLogIn] = useState(true);
+
+  const handleLoginClick = () => {
+    setLogIn(true);
+    setRegister(false);
+  };
+
+  const handleRegisterClick = () => {
+    setRegister(true);
+    setLogIn(false);
+  };
+
+  console.log("login :", login, "register :", register);
+
   return (
     <section className="loginForm">
       <div className=" container row">
         <div className="col-sm-12 col-md-8 col-lg-5">
           <div className="header">
-            <h2 className="h4">Login/Register</h2>
+            <div>
+              <button className="login" onClick={handleLoginClick}>
+                Login
+              </button>
+            </div>
+            <div>
+              <button className="register" onClick={handleRegisterClick}>
+                Register
+              </button>
+            </div>
           </div>
           <form className="form">
+            <legend>{login ? "Login" : "Register"}</legend>
             <label htmlFor="email"></label>
             <input
               type="text"
