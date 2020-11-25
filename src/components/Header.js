507ef2babb4732badab0c5186bf160001191d214
pierre-phil <pierre-philippon@live.fr>
2020-11-25 17:47:19 +0100

    try logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import { IsLoggedInContext } from "../context/IsLoggedInContext";
 
+import Logo from "../images/logo.png";
+
 const Header = () => {
   const { isLoggedIn } = useContext(IsLoggedInContext);
   console.log("isLoggedIn", isLoggedIn);
@@ -13,6 +15,7 @@ const Header = () => {
         <h1 className="h2">
           <Link to="/">Name of App</Link>
         </h1>
+        {/*<img src={Logo} alt="..."></img>*/}
         <nav>
           <Link to="/reservation">Reservation</Link>
           <Link to="/travels">Travels</Link>
